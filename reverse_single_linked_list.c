43ee8ff0a222bc2680059854ebee8bfeb659becf
Noorain-Malik <maliknoorain99@gmail.com>
2025-10-04 11:49:53 +0530

    Add files via upload

---
+++ reverse_single_linked_list.c
@@ -0,0 +1,92 @@
+
+#include <stdio.h>
+#include<stdlib.h>
+struct node{
+    int data;
+    struct node* next;
+};
+struct node* create_node(int n){
+    struct node* newnode=(struct node*)malloc(sizeof(struct node));
+    newnode->data=n;
+    newnode->next=NULL;
+    return newnode;
+    
+}
+struct node* createlist(int size){
+    struct node *head=NULL,*temp=NULL,*newnode=NULL;
+    int value;
+    for(int i=0;i<size;i++){
+      printf("enter the value for %d node",i+1);
+      scanf("%d",&value);
+      newnode=create_node(value);
+      if(head==NULL){
+        head=newnode;
+        temp=head;
+      }
+      else{
+        temp->next=newnode;
+        temp=newnode;
+      }
+    }
+    return head;
+}
+void display_linked_list(struct node* ptr){
+while(ptr!=NULL){
+    printf(" %d\n",ptr->data);
+    ptr=ptr->next;
+}
+}
+void freeList(struct node* head) {
+    struct node* temp;
+    while (head != NULL) {
+        temp = head;
+        head = head->next;
+        free(temp);
+    }
+}
+struct node* reverse(struct node* head){
+    struct node* temp1=head,*prev=head,*head2=head,*temp2=head2;
+    while(head->next!=NULL){
+        temp1=head;
+        prev=NULL;
+        while(temp1->next!=NULL){
+            prev=temp1;
+            temp1=temp1->next;
+        }
+        prev->next=NULL;
+        
+       if(head2==head){
+            head2=temp1;
+            temp2=head2;
+        }
+        else{
+            temp2->next=temp1;
+            temp2=temp1;
+        }
+        
+    }
+    if(temp1==head){
+        temp1->next=NULL;
+    }
+    else if(prev==head){
+           temp2->next = head;   // attach last node to reversed list
+           temp2=temp2->next;
+           temp2->next=NULL;
+        }
+
+    return head2;
+}
+
+int main(){
+    int n;
+    printf("How many nodes do you want? ");
+    scanf("%d", &n);
+
+    struct node* head = createlist(n);
+    display_linked_list(head);
+    head=reverse(head);
+    display_linked_list(head);
+    
+    freeList(head);
+    return 0;
+}
